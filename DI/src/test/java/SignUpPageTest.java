23dca2c73562cde241028180fc63c466c12428c1
VallKillmar <valentin.an.varbanov@gmail.com>
2024-09-09 14:54:44 +0300

    Implementing PageFactory to the project final

--- DI/src/test/java/SignUpPageTest.java
+++ DI/src/test/java/SignUpPageTest.java
@@ -1,13 +1,8 @@
-import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import pageObjects.LoginForm;
-import pageObjects.SignUpForm;
-import pages.BasePage;
-import pages.LoginPage;
 import pages.SignUpPage;
 
-public class SignUpTest extends BaseTest{
+public class SignUpPageTest extends BaseTest{
 
     @Test
     public void enterSignUpForm(){
