35bcf997f6b59dcac8a712ee846b9f677f5b6340
VallKillmar <valentin.an.varbanov@gmail.com>
2024-09-25 21:19:56 +0300

    adding waits to the post an image test

--- DI/src/test/java/PostPictureTest.java
+++ DI/src/test/java/PostPictureTest.java
@@ -6,13 +6,27 @@ import pages.NewPostPage;
 public class PostPictureTest extends BaseTest{
 
     @Test
-    public void testPostAnImage() throws InterruptedException {
+    public void testPostPrivateImage(){
         loginPage.loginToTheSystem();
         NewPostPage newPostPage = new NewPostPage(webDriver);
         newPostPage.waitToastValidationMessageInvisibility();
         newPostPage.getFullHeader().clickOnNewPostButtonElement();
         newPostPage.getPostAPictureForm().sendFileToPictureInputElement();
         newPostPage.getPostAPictureForm().clickOnPrivatePublicSwitchElement();
+        newPostPage.getPostAPictureForm().clickOnSubmitButtonElement();
+        CommonPage commonPage = new CommonPage(webDriver);
+        commonPage.waitToastValidationMessageVisibility();
+        String actualToastValidationMessage = commonPage.getToastValidationMessageElementText();
+        Assert.assertEquals(actualToastValidationMessage, "Post created!");
+    }
+
+    @Test
+    public void testPostPublicImage(){
+        loginPage.loginToTheSystem();
+        NewPostPage newPostPage = new NewPostPage(webDriver);
+        newPostPage.waitToastValidationMessageInvisibility();
+        newPostPage.getFullHeader().clickOnNewPostButtonElement();
+        newPostPage.getPostAPictureForm().sendFileToPictureInputElement();
         newPostPage.getPostAPictureForm().clickOnSubmitButtonElement();
         CommonPage commonPage = new CommonPage(webDriver);
         commonPage.waitToastValidationMessageVisibility();
